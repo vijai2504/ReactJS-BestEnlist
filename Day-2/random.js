96042c9307c9c79d453fe59cb856652a44299055
vijai2504 <vijaymsd2404@gmail.com>
2020-11-28 20:55:42 +0530

    Day-2

---
+++ Day-2/random.js
@@ -0,0 +1,1 @@
+Math.floor((Math.random()*1000000)+1);
---
+++ Day-2/square-fun.js
@@ -0,0 +1,7 @@
+function sumOfSquares(num) {
+    var i;
+    i=num*num;
+    console.log("The Square for", num, "is", i );
+}
+var num = prompt("Enter the number");
+sumOfSquares(num);
