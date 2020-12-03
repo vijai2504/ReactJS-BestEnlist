ac8b6c47744ee2ed5ef1df84531b3a245170d151
vijai2504 <vijaymsd2404@gmail.com>
2020-12-03 23:51:00 +0530

    Day-7

---
+++ Day-7/beaddobj.js
@@ -0,0 +1,13 @@
+Array.prototype.beaddobject = function (key, value) {
+    this.map((object) => {
+        object[key] = value;
+    });
+    return this;
+}
+
+var sampleinput = [
+    { name: "John", age: 26 },
+    { name: "Siva", age: 30 },
+];
+
+console.log(sampleinput.beaddobject("Country", "India"));
