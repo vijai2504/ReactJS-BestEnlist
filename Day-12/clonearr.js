1f6e3d389fd89534d92cc4488a5d7e9bf72511cb
VIJAI K A <60223354+vijai2504@users.noreply.github.com>
2020-12-08 19:56:47 +0530

    Revert "Day-11 & Day-12"
    This reverts commit 3c05e448d70ab5ec09e96e11629026e12e433b4f.

--- Day-12/clonearr.js
+++
@@ -1,8 +0,0 @@
-function arrayClone (newArray){
-    return newArray.slice(); 
-}
-
-newArray = [1, 2, 3, 4, 5];
-
-console.log("newArray -- ", newArray);
-console.log("clonedArray -- ", arrayClone(newArray));
